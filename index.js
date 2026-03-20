b64c6ea9458224952286256a44cd55d5919d065b
ast6321 <tanwarji6321@gmail.com>
2026-03-20 16:01:18 +0530

    complete the crud operation apis with integrating data to mongodb

--- index.js
+++ index.js
@@ -43,3 +43,82 @@ app.get("/users/:id",async(req,res)=>{
          res.status(500).json({message:err.message});
     }
 })
+
+
+app.post("/users/new",async(req,res)=>{
+
+    try{
+      const data = req.body;
+      const lastuser =  await user.findOne().sort({trainee_id:-1});
+      let newid = 1;
+      if(lastuser){
+           newid = lastuser.trainee_id + 1;
+      }
+     
+       
+         
+      const newuser = new user({...data,trainee_id:newid});
+
+        await newuser.save();
+        res.json("user added successfuly")
+
+    }
+    catch(err){
+         res.status(500).json({message:err.message});
+    }
+})
+
+app.put("/users/:id",async(req,res)=>{
+    try{
+        const id = Number(req.params.id);
+        const data = req.body;
+        data.trainee_id = id;
+
+        const updateuser = await user.findOneAndUpdate({trainee_id:id},data,{returnDocument: "after",runValidators:true});
+         
+      if(!updateuser){
+        return res.status(404).json({ message: "User not found" });
+      }
+
+       res.json({message: "User updated successfully"});
+
+
+    }
+     catch(err){
+         res.status(500).json({message:err.message});
+    }
+
+})
+
+
+app.patch("/users/:id",async(req,res)=>{
+    try{
+        const id = Number(req.params.id);
+        const data = req.body;
+        const updateuser = await user.findOneAndUpdate({trainee_id:id},data,{returnDocument:"after"});
+        if(!updateuser){
+            res.status(404).json("user not found");
+        }
+        res.json("details updated");
+
+    }
+    catch(err){
+         res.status(500).json({message:err.message});
+    }
+})
+
+app.delete("/users/:id",async(req,res)=>{
+    try{
+        const id = Number(req.params.id);
+        const deleteduser = await user.findOneAndDelete({trainee_id:id});
+
+        if(!deleteduser){
+            res.status(404).json("user not found");
+        }
+
+        res.json("user deleted");
+    }
+    catch(err){
+         res.status(500).json({message:err.message});
+    }
+})
